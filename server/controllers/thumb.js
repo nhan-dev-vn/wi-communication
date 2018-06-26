0456000e6d8c14d743a99d8b0c8562c2af53e094
nhannt97 <nhannt97hy@gmail.com>
2018-06-26 17:14:33 +0700

    update

--- server/controllers/thumb.js
+++ server/controllers/thumb.js
@@ -20,31 +20,31 @@ module.exports.thumb = function(req, res) {
             });
         }else{
             console.log(original_dir, thumb_dir);
-            thumb(req, res, original_dir, thumb_dir);
+            // thumb(req, res, original_dir, thumb_dir);
+            thumb({
+                source: original_dir,
+                destination: thumb_dir,
+                basename: md5(req.params.fileName),
+                suffix: '',
+                width: WIDTH_IMAGE_THUMB,
+                skip: true,
+                overwrite: false
+            }, function(files, err, stdout, stderr){
+                if(err || stderr) {
+                    res.send(response(400, 'THUMB FAIL', err));
+                }
+                else {
+                    var lastDots = req.params.fileName.lastIndexOf('.');
+                    var typeFile = req.params.fileName.substring(lastDots, req.params.fileName.length);
+                    var pathOutFile = path.join(thumb_dir, md5(req.params.fileName) + typeFile);
+                    console.log(pathOutFile);
+                    res.sendFile(pathOutFile);
+                }
+            });
         }
     });
     
 }
 function thumb(req, res, original_dir, thumb_dir){
     console.log(original_dir, thumb_dir);
-    thumb({
-        source: original_dir,
-        destination: thumb_dir,
-        basename: md5(req.params.fileName),
-        suffix: '',
-        width: WIDTH_IMAGE_THUMB,
-        skip: true,
-        overwrite: false
-    }, function(files, err){
-        if(err) {
-            res.send(response(400, 'THUMB FAIL', err));
-        }
-        else {
-            var lastDots = req.params.fileName.lastIndexOf('.');
-            var typeFile = req.params.fileName.substring(lastDots, req.params.fileName.length);
-            var pathOutFile = path.join(thumb_dir, md5(req.params.fileName) + typeFile);
-            console.log(pathOutFile);
-            res.sendFile(pathOutFile);
-        }
-    });
 }
